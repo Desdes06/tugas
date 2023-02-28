daf43e16b0fe3c31f74c4d1ae8259dbada745f6d
Desdes06 <122344130+Desdes06@users.noreply.github.com>
2023-02-28 14:55:14 +0700

    Add files via upload

---
+++ paket.py
@@ -0,0 +1,13 @@
+from pkg.modul1 import *
+from pkg.modul2 import *
+from pkg.hitung_nilai import *
+
+def main():
+    f1()
+    f2()
+    f3()
+    f4()
+    print("Nilai Akhir :", hitung_nilai(80, 78))
+    print("Hasil :", hitung(80, 78))
+
+main()
